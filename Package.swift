09a340202e81c96f4c00fb9d1dce9f7fa300f912
Octavio Ruiz <octavio@servicios-corporativos.com>
2020-05-23 11:19:46 -0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -17,6 +17,7 @@ let package = Package(
     targets: [
         .target(
             name: "IntroScreen",
+            path: "IntroScreen",
             dependencies: []),
         .testTarget(
             name: "IntroScreenTests",
